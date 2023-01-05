1754ad7b4b1fb5c6e2e2985874a945e891139bad
felixokolo <pheelix.info@gmail.com>
2023-01-05 14:52:19 +0100

    First commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Annotated addition function
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats
+
+    Parameters:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        sum (float): sum of a and b
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Annotated concat function
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concats str1 and str2
+
+    Parameters:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        concat (str): concat of str1 and str2
+    """
+    return str1 + str2
