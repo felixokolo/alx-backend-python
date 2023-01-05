5d004d8830142fea04094049ba1dc166c362608e
felixokolo <pheelix.info@gmail.com>
2023-01-05 23:35:02 +0100

    corrections

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -17,11 +17,5 @@ def to_kv(k: str,
         v (number): tuple float or int
     Returns:
         tuple (k, v)
-                                    Union[int,
-                                          
     """
     return (k, v ** 2)
-
-print(to_kv.__annotations__)
-print(to_kv("eggs", 3))
-print(to_kv("school", 0.02))
