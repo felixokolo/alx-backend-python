2eaea719d6073645523f60caf1963b1a7fe68675
felixokolo <pheelix.info@gmail.com>
2023-01-05 23:33:50 +0100

    corrections

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,9 +8,7 @@ string iint/float to tuple
 
 def to_kv(k: str,
           v: Union[int,
-                   float]) -> Tuple[str,
-                                    Union[int,
-                                          float]]:
+                   float]) -> Tuple[str, float]:
     """
     To tuple
 
@@ -19,5 +17,11 @@ def to_kv(k: str,
         v (number): tuple float or int
     Returns:
         tuple (k, v)
+                                    Union[int,
+                                          
     """
     return (k, v ** 2)
+
+print(to_kv.__annotations__)
+print(to_kv("eggs", 3))
+print(to_kv("school", 0.02))
