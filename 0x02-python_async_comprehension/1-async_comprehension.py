365f097534383cfd81662d1d80ed3ee1625a411f
felixokolo <pheelix.info@gmail.com>
2023-01-10 20:54:11 +0100

    addition

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+
+import random
+import asyncio
+from typing import AsyncGenerator
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> list:
+    """
+    Asynchronous generator function
+
+    Returns:
+        random number
+    """
+
+    return [x async for x in async_generator()]
